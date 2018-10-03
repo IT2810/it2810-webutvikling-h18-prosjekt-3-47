4b8074b36d1e4e92d94c904f422b5739d1fc132e
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-10-03 16:16:49 +0200

    #5 har lagt til noen eksempeltester med kode i testing mappa

--- testing/functions.test.js
+++ testing/functions.test.js
@@ -1,5 +1,33 @@
 const functions = require('./functions');
 
+//Før enhver
+//beforeEach(()=> initDatabase());
+//Etter enhver
+//afterEach(() => closeDatabase());
+
+//Før alle
+//beforeAll(()=> initDatabase());
+//Etter alle
+//afterAll(() => closeDatabase());
+
+//const initDatabase = () => console.log("Database Initialized...");
+//const closeDatabase = () => console.log("Database Closed...");
+const nameCheck = () => console.log("Checking Name...")
+
+describe('Ckecking Names', () => {
+    beforeEach(() => nameCheck());
+
+    test('User is Jeff', () => {
+        const user = 'Jeff';
+        expect(user).toBe('Jeff');
+    });
+
+    test('User is Karen', () => {
+        const user = 'Karen';
+        expect(user).toBe('Karen');
+    });
+});
+//Feil i testsuite == du har ikke en test i testfila di ;)
 //To be
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4);
@@ -10,7 +38,7 @@ test('Adds 2 + 2 to NOT equal 5', () => {
     expect(functions.add(2,2)).not.toBe(5);
 });
 
-
+/*
 //To be null
 test('Should be null', () => {
     expect(functions.isNull()).toBeNull();
@@ -46,12 +74,23 @@ test('Admin should be in usernames', () =>{
     usernames = ['John','Karen','Admin']
     expect(usernames).toContain('Admin')
 })
-
+*/
 //Working with async data
+
+//Promise
+/*
 test('User fetched name should be Leanne Graham', () => {
-    //expect.assertions(1);
+    expect.assertions(1);
     return functions.fetchUser()
     .then(data => {
         expect(data.name).toEqual('Leanne Graham');
     })
-})
+})*/
+
+//Async await
+/*
+test('User fetched name should be Leanne Graham', async () => {
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+})*/
