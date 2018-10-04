5646a577e6058d215fcc523ea00d0069ffa17f98
Sigurd Øines <sigurdoines@gmail.com>
2018-10-05 00:17:21 +0200

    feat: Now loads events from AsyncStorage, makes markings that react-native-calendar likes. #1

--- components/Calendar/Calendar.js
+++ components/Calendar/Calendar.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {ScrollView, StyleSheet, View, Button, DatePickerAndroid, Platform, Text, AsyncStorage} from 'react-native';
-import { Calendar, Agenda, LocaleConfig} from 'react-native-calendars';
+import {StyleSheet, View, Button, DatePickerAndroid, Platform, AsyncStorage} from 'react-native';
+import { Calendar, LocaleConfig} from 'react-native-calendars';
 import DayView from './DayView'
 import moment from 'moment';
 import 'moment/locale/nb';
 
-export default class CalendarScreen extends React.Component {
-    static navigationOptions = {
-        title: 'Calendar',
-    };
+export default class CalendarComponent extends React.Component {
 
     colors = ['green', 'blue', 'grey', 'orange', 'salmon', 'red'];
+    df = 'YYYY-MM-DD';
 
     constructor (props){
         super(props);
@@ -19,16 +17,10 @@ export default class CalendarScreen extends React.Component {
         moment.locale('nb');
 
         this.state = {
-            view: 'Calendar',
             now: moment(),
             selected: moment(),
-            events: {
-                '2018-10-02': [{text: 'Møte med Per'}],
-                '2018-10-03': [{text: 'Møte med Pål'}],
-                '2018-10-04': [],
-                '2018-10-05': [{text: 'Møte med Espen'},{text: 'Møte med Jens'}, {text: 'Møte med Siri'},{text: 'Møte med Gunvor'}],
-                '2018-11-02': [{text: 'Møte med Arild'}]
-            }
+            events: {},
+            eventMarkers: {}
         };
 
         this.subtractMonth = this.subtractMonth.bind(this);
@@ -40,10 +32,12 @@ export default class CalendarScreen extends React.Component {
     _retrieveData = async () => {
         try {
             const value = await AsyncStorage.getItem('events');
-            console.log('data exists, might be null');
             if (value !== null) {
                 console.log('We have data!!');
-                console.log(value);
+
+                this.setState(() => ({
+                    events: JSON.parse(value)
+                }));
             }
         } catch (error) {
             console.error('Error reading from AsyncStorage', error);
@@ -59,63 +53,84 @@ export default class CalendarScreen extends React.Component {
     };
 
     _storeData = async () => {
+        console.log('halo');
         try {
             await AsyncStorage.setItem('events', JSON.stringify(this.events));
+            console.log('Wrote to AsyncStorage');
         } catch (error) {
             console.error('Error storing to AsyncStorage', error);
         }
     };
 
+    getDaysInMonth(date) {
+        let days = [];
 
-    generateEventMarkers() {
+        let day = moment(date.format('YYYY-MM'), 'YYYY-MM');
+        while (day.format('MM') === date.format('MM')) {
+            days.push(day.format(this.df));
+            day.add(1, 'day');
+        }
+        return days;
+    }
+
+
+    generateEventMarkers(events) {
         let eventMarkers = {};
-        let i = 19;
-        for (let key of Object.keys(this.state.events)) {
+        let i = 0; // Counter to keep colours somewhat different
+
+        // The calendar only considers dates as loaded if they have an entry in markedDates
+        for (let key of this.getDaysInMonth(this.state.selected)){
             eventMarkers[key] = {};
             if(this.formattedString(this.state.selected) === key) {
                 eventMarkers[key].selected = true;
-                eventMarkers[key].selectedColor = 'blue';
-            } else {
-                eventMarkers[key].disabled = true;
             }
+
             let dots = [];
             let j = 0;
-            for (let item of this.state.events[key]) {
-                if (this.formattedString(this.state.selected) === key) {
-                    dots.push({key: key + '_' + j, color: this.colors[i % this.colors.length], selected: true, selectedDotColor: 'red'})
-                } else {
-                    dots.push({key: key + '_' + j, color: this.colors[i % this.colors.length]})
-                }
-                j++;
-                i++;
+
+            if (events.hasOwnProperty(key)){
+                events[key].forEach(()=> {
+                    if (this.formattedString(this.state.selected) === key) {
+                        dots.push({key: key + '_' + j, color: this.colors[i % this.colors.length], selected: true})
+                    } else {
+                        dots.push({key: key + '_' + j, color: this.colors[i % this.colors.length]})
+                    }
+                    j++;
+                    i++;
+                });
             }
 
             eventMarkers[key].dots = dots;
         }
         // console.log(eventMarkers);
+
         return eventMarkers;
     }
 
     subtractMonth() {
         this.setState(prevState => ({
-            selected: prevState.selected.add(-1, 'month')
-        }), console.log('updated:', this.formattedString(this.state.selected)));
+            selected: this.dateCopy(prevState.selected).subtract(1, 'month')
+        }), console.log('updated, subtracted from selected month:', this.formattedString(this.state.selected)));
     }
 
     addMonth() {
         this.setState(prevState => ({
-            selected: prevState.selected.add(1, 'month')
-        }), console.log('updated:', this.formattedString(this.state.selected)));
+            selected: this.dateCopy(prevState.selected).add(1, 'month')
+        }), console.log('updated, subtracted from selected month:', this.formattedString(this.state.selected)));
     }
 
-    formattedString(moment) {
-        return moment.format('YYYY-MM-DD');
+    formattedString(date) {
+        return date.format(this.df);
+    }
+
+    dateCopy(date) {
+        return moment(this.formattedString(date), this.df)
     }
 
     setSelected(dateString) {
         this.setState({
-            selected: moment(dateString, "YYYY-MM-DD")
-        }, console.log('selected set:', this.formattedString(this.state.selected)));
+            selected: moment(dateString, this.df)
+        }, console.log('updated, selected set:', this.formattedString(this.state.selected)));
     }
 
     async openDatePicker (){
@@ -138,13 +153,46 @@ export default class CalendarScreen extends React.Component {
 
     }
 
+    componentDidMount() {
+        console.log('helu');
+        // Need to write to AsyncStorage on each mount, since it does not persist in dev mode
+
+        this._storeData().then(() => {
+            this._retrieveData()
+        });
+    }
+
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate!!!!');
+        let toUpdate = {};
+
+
+        if (prevState.events !== this.state.events){
+            // New events received
+            console.log('new events received');
+
+            toUpdate.eventMarkers = this.generateEventMarkers(this.state.events);
+
+        } else if (prevState.selected.format(this.df) !== this.state.selected.format(this.df)){
+            // Selected date changed
+
+            // TODO: Might add a separate updateEventMarkers here, will be way more efficient
+            toUpdate.eventMarkers = this.generateEventMarkers(this.state.events);
+        }
+
+
+
+        if (Object.keys(toUpdate).length !== 0) {
+            console.log('Something to update');
+            this.setState(toUpdate);
+
+        } else console.log('Nothing to update');
+    }
+
     render() {
 
-        this._storeData();
-
-        //this._retrieveData();
-
-        console.log(this.formattedString(this.state.selected));
+        console.log('rendering', this.formattedString(this.state.selected));
         /*
            Reference for react-native-calendars: https://github.com/wix/react-native-calendars
          */
@@ -158,14 +206,12 @@ export default class CalendarScreen extends React.Component {
 
         LocaleConfig.defaultLocale = 'no';
 
-        let calendar = null;
-
-        if (this.state.view === 'Calendar') {
-            calendar =
+        return (
+            <View style={styles.container}>
                 <Calendar
                     current={this.formattedString(this.state.selected)}
 
-                    markedDates={this.generateEventMarkers()}
+                    markedDates={this.state.eventMarkers}
 
                     markingType={'multi-dot'}
 
@@ -181,10 +227,13 @@ export default class CalendarScreen extends React.Component {
                     // Month format in calendar title. Formatting values: http://arshaw.com/xdate/#Formatting
                     monthFormat={'MMMM yyyy'}
                     // Handler which gets executed when visible month changes in calendar. Default = undefined
+
+                    /* Seems redundant
                     onMonthChange={(month) => {
                         console.log('month changed', month);
                         this.setSelected(month.dateString);
                     }}
+                    */
                     // Hide month navigation arrows. Default = false
                     hideArrows={false}
                     // If firstDay=1 week starts from Monday. Note that dayNames and dayNamesShort should still start from Sunday.
@@ -197,86 +246,18 @@ export default class CalendarScreen extends React.Component {
                     onPressArrowLeft={this.subtractMonth}
                     // Handler which gets executed when press arrow icon left. It receive a callback can go next month
                     onPressArrowRight={this.addMonth}
-                />;
-        } else if (this.state.view === 'Agenda') {
-            calendar =
-                <Agenda
-                    firstDay={1}
-                    // the list of items that have to be displayed in agenda. If you want to render item as empty date
-                    // the value of date key kas to be an empty array []. If there exists no value for date key it is
-                    // considered that the date in question is not yet loaded
-                    items={this.state.events}
-                    // callback that gets called when items for a certain month should be loaded (month became visible)
-                    loadItemsForMonth={(month) => {console.log('trigger items loading')}}
-                    // callback that fires when the calendar is opened or closed
-                    onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
-                    // callback that gets called on day press
-                    onDayPress={(day)=>{console.log('day pressed')}}
-                    // callback that gets called when day changes while scrolling agenda list
-                    onDayChange={(day)=>{console.log('day changed')}}
-                    // initially selected day
-                    selected={'2018-10-01'}
-                    // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-                    minDate={'2018-10-01'}
-                    // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-                    maxDate={'2018-10-07'}
-                    // Max amount of months allowed to scroll to the past. Default = 50
-                    pastScrollRange={50}
-                    // Max amount of months allowed to scroll to the future. Default = 50
-                    futureScrollRange={50}
-                    // specify how each item should be rendered in agenda
-                    renderItem={(item, firstItemInDay) => {
-                        console.log(item);
-                        return (
-                            <View style={[styles.item, {height: item.height}]}><Text>{item.text}</Text></View>
-                        );
-                    }}
-                    // specify how each date should be rendered. day can be undefined if the item is not first in that day.
-                    renderDay={(day, item) => {
-                        console.log('day: ', day);
-                        console.log('item: ', item);
-                        if (item){
-                            return(
-                                <View><Text>{day ? day.day: 'a'}</Text></View>
-                            )
-                        }
-                        return undefined;
-                    }}
-                    // specify how empty date content with no items should be rendered
-                    renderEmptyDate={() => {return (<View style={styles.emptyDate}><Text>This is empty date!</Text></View>);}}
-                    // specify how agenda knob should look like
-                    renderKnob={() => {return ( <View />);}}
-                    // specify what should be rendered instead of ActivityIndicator
-                    renderEmptyData = {() => {return (<View />);}}
-                    // specify your item comparison function for increased performance
-                    rowHasChanged={(r1, r2) => {return r1.text !== r2.text}}
-
-                    // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
-                    onRefresh={() => console.log('refreshing...')}
-                    // Set this true while waiting for new data from a refresh
-                    refreshing={false}
-                    // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
-                    refreshControl={null}
-                    // agenda theme
-                    theme={{
-                        agendaDayTextColor: 'yellow',
-                        agendaDayNumColor: 'green',
-                        agendaTodayColor: 'red',
-                        agendaKnobColor: 'blue'
-                    }}
-                    // agenda container style
-                    style={{}}
-                />;
-        }
-
-        return (
-            <View style={styles.container}>
-                {calendar}
-                <Button
+                />
+                <Button style={styles.button}
                     onPress={this.openDatePicker}
                     title="Open date picker"
                     color="#841584"
                     accessibilityLabel="Open the date picker"
+                />
+                <Button style={styles.button}
+                    onPress={() => {this.setSelected(moment().format(this.df))}}
+                    title="Today"
+                    color="#841584"
+                    accessibilityLabel="Set calendar to today"
                 />
                 <DayView day={this.state.selected.format('Do')}
                          events={this.state.events[this.formattedString(this.state.selected)]}
@@ -304,5 +285,8 @@ const styles = StyleSheet.create({
         height: 15,
         flex:1,
         paddingTop: 30
+    },
+    button: {
+
     }
 });
