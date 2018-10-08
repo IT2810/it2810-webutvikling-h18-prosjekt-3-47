9be017c3d9c3945642bee521035eafa64faaf659
Jørgen Buholm Johansen <jorgenbj@stud.ntnu.no>
2018-10-08 10:34:31 +0200

    chore: har lagt til newlines #5

--- testing/chunk.test.js
+++ testing/chunk.test.js
@@ -9,4 +9,4 @@ test('Chunk an array of 3 values with length of 1', () => {
     const len = 1;
     const chunkedArr = chunkArray(numbers, len);
     expect(chunkedArr).toEqual([[1],[2],[3]]);
-})
+});
