b4094adca6c9ba3eba216eb1009590fc723335da
Sigurd Øines <sigurdoines@gmail.com>
2018-10-19 11:20:53 +0200

    chore: removed comment, added missing semicolon. #19

--- screens/TodolistScreen.js
+++ screens/TodolistScreen.js
@@ -7,8 +7,6 @@ export default class TodolistScreen extends React.Component {
   };
 
   render() {
-    /* Go ahead and delete ExpoConfigView and replace it with your
-     * content, we just wanted to give you a quick view of your config */
     return <Todolist />;
   }
 }
