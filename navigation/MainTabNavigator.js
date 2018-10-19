08c39673689bd96ecaf47a4d341792bc3e18cc3e
Sigurd Øines <sigurdoines@gmail.com>
2018-10-19 16:32:49 +0200

    chore: removed example pages, rewrote HomeScreen to show a welcome message. #19

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -4,8 +4,6 @@ import { createStackNavigator, createBottomTabNavigator } from 'react-navigation
 
 import TabBarIcon from '../components/TabBarIcon';
 import HomeScreen from '../screens/HomeScreen';
-import LinksScreen from '../screens/LinksScreen';
-import SettingsScreen from '../screens/SettingsScreen';
 import GeolocationScreen from '../screens/GeolocationScreen';
 import ContactsScreen from '../screens/ContactsScreen';
 import CalendarScreen from '../screens/CalendarScreen';
@@ -29,35 +27,6 @@ HomeStack.navigationOptions = {
     />
   ),
 };
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-LinksStack.navigationOptions = {
-  tabBarLabel: 'Links',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-link${focused ? '' : '-outline'}` : 'md-link'}
-    />
-  ),
-};
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
-SettingsStack.navigationOptions = {
-  tabBarLabel: 'Settings',
-  tabBarIcon: ({ focused }) => (
-    <TabBarIcon
-      focused={focused}
-      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
-    />
-  ),
-};
-
 
 const GeolocationStack = createStackNavigator({
   Geolocation: GeolocationScreen,
@@ -120,11 +89,9 @@ TodolistStack.navigationOptions = {
 };
 
 export default createBottomTabNavigator({
+  HomeStack,
   GeolocationStack,
   CalendarStack,
   ContactsStack,
-  TodolistStack,
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+  TodolistStack
 });
